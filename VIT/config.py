a37f9ad2fb75d783ff9d3d92dfb397a6cd95b4bc
Danielement321 <danielement321@gamil.com>
2025-02-26 17:14:43 +0800

    Improve code structure for VIT

--- VIT/config.py
+++ VIT/config.py
@@ -1,24 +1,31 @@
 import torch
 from transformers import PretrainedConfig
 
-CONFIG = {
-    'd_model': 768,
-    'max_seq_length': 64,
-    'num_heads': 12,
-    'encoder_depth': 6, # This is ignored if you use DecoderOnly Setting.
-    'decoder_depth': 12,
-    'ffn_dim': 3072,
-    'dropout': 0.1,
-    'vocab_size': 999999,
-    'eps': 1e-6,
-    'device': 'cuda' if torch.cuda.is_available() else 'cpu',
-    'tokenizer': 'google-bert/bert-base-uncased',
-}
 
-VIT_CONFIG = {
-    'patch_size': 14,
-    'image_size': 224,
-    'in_channels': 3,
-    'device': 'cuda',
-    'transformer_config': CONFIG,
-}
+class SimpleVITConfig(PretrainedConfig):
+    model_type = 'simple_vit'
+    
+    def __init__(self,
+                 hidden_size = 768,
+                 num_attention_heads = 12,
+                 n_layers = 6,
+                 intermediate_size = 3072,
+                 dropout = 0.1,
+                 patch_size = 14,
+                 image_size = 224,
+                 in_channels = 3,
+                 flash_attn = True,
+                 eps = 1e-6,
+                 **kwargs):
+        self.hidden_size = hidden_size
+        self.num_attention_heads = num_attention_heads
+        self.n_layers = n_layers
+        self.intermediate_size = intermediate_size
+        self.dropout = dropout
+        self.patch_size = patch_size
+        self.image_size = image_size
+        self.in_channels = in_channels
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.flash_attn = flash_attn
+        self.eps = eps
+        super().__init__(**kwargs)
